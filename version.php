405cb070ef414f4f58adf85b7d06bccb57212bb4
Rogier van Dongen <rogier@sebsoft.nl>
2025-12-08 15:21:36 +0100

    Fix Incorrect syntax in plugin supported declaration in usersuspension
    and update patch version

--- version.php
+++ version.php
@@ -29,15 +29,14 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin = new stdClass();
-$plugin->version   = 2025120100;
+$plugin->version   = 2025120101;
 $plugin->requires  = 2024042200;      // YYYYMMDDHH (This is the release version for Moodle 4.4).
 $plugin->cron      = 0;
 $plugin->component = 'tool_usersuspension'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '4.0.6 (build 2025120100)';
+$plugin->release   = '4.0.6.1 (build 2025120101)';
 $plugin->dependencies = [];
 $plugin->supported = [
     404,
-    405,
     500,
 ];
